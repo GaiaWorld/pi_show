d69359a85708c9d1f33aee24eaf81d9e203625f7
wzjsun <530739162@qq.com>
2019-04-11 11:21:34 +0800

    测试

--- gui/src/test/bind.rs
+++ gui/src/test/bind.rs
@@ -30,13 +30,13 @@ pub fn test(){
     append_child(world, 1, node2);
     set_width(world, node2, 1000.0);
     set_height(world, node2, 700.0);
-    set_backgroud_rgba_color(world, node2, 1.0, 0.0, 0.0, 0.5);
+    set_backgroud_rgba_color(world, node2, 1.0, 0.0, 0.0, 1.0);
 
     let node3 = create_node(world);
     append_child(world, node2, node3);
     set_width(world, node3, 500.0);
     set_height(world, node3, 500.0);
-    set_backgroud_rgba_color(world, node3, 0.0, 0.0, 1.0, 1.0);
+    set_backgroud_rgba_color(world, node3, 0.0, 0.0, 1.0, 0.5);
     set_border_color(world, node3, 0.0, 1.0, 0.0, 1.0);
     set_border(world, node3, unsafe{transmute(YGEdge::YGEdgeLeft)}, 10.0);
     set_border(world, node3, unsafe{transmute(YGEdge::YGEdgeRight)}, 10.0);
