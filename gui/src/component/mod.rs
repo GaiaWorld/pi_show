5ed63279d44592bfbe38a1de5b51f27dd84a62e4
zmax <zmythleo@gmail.com>
2019-05-16 13:45:50 +0800

    add vector4

--- gui/src/component/mod.rs
+++ gui/src/component/mod.rs
@@ -8,6 +8,7 @@ pub type Point2 = cgmath::Point2<f32>;
 pub type Point3 = cgmath::Point3<f32>;
 pub type Vector2 = cgmath::Vector2<f32>;
 pub type Vector3 = cgmath::Vector3<f32>;
+pub type Vector4 = cgmath::Vector4<f32>;
 pub type Aabb3 = collision::Aabb3<f32>;
 
 #[derive(Clone, Copy, Debug)]
