e0f5de7677a5272df0f59bd79d2560fb8f1eadc2
wzjsun <530739162@qq.com>
2019-06-15 16:51:22 +0800

    info

--- gui/src/bind/debug.rs
+++ gui/src/bind/debug.rs
@@ -9,6 +9,7 @@ use component::user::*;
 use component::calc::*;
 use component::calc::Opacity as COpacity;
 use single::{ OverflowClip, RenderObjs, DefaultTable};
+use single::oct::Oct;
 use entity::Node;
 use system::util::cal_matrix;
 // use layout::YgNode;
@@ -102,9 +103,13 @@ pub fn node_info(world: u32, node: u32) {
         content_box: absolute_c_box,
     };
 
-    // let yogas = world.fetch_multi::<Node, YgNode>().unwrap();
-    // let yogas = yogas.lend();
-    // let yoga = unsafe { yogas.get_unchecked(node) };
+    let yogas = world.fetch_multi::<Node, YgNode>().unwrap();
+    let yogas = yogas.lend();
+    let yoga = unsafe { yogas.get_unchecked(node) };
+
+    let octs = world.fetch_single::<Oct>().unwrap();
+    let octs = octs.lend();
+    let oct = unsafe { octs.get_unchecked(node) };
 
     js!{
         window.__jsObj = @{info};
@@ -112,6 +117,7 @@ pub fn node_info(world: u32, node: u32) {
         // console.log("node_info:", window.__jsObj);
         // console.log("style:", @{format!( "{:?}", yoga.get_style() )});
         // console.log("layout:", @{format!( "{:?}", yoga.get_layout() )});
+        // console.log("boundBox:", @{format!( "{:?}", oct )});
     }
 }
 
