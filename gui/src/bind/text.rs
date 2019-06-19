a0535e66a5b52a2b8ff81558481983da728f08e6
wzjsun <530739162@qq.com>
2019-06-19 11:40:27 +0800

    去除打印

--- gui/src/bind/text.rs
+++ gui/src/bind/text.rs
@@ -447,7 +447,6 @@ fn look_text(world_id: u32, node: usize, text: &str){
             let chars: Vec<u32> = unsafe {transmute(chars)};
         
             if r.get_dyn_type() == 0 {
-                println!("r.get_dyn_type() == 0 ---------------------------");
                 let gl = gen_font(world_id, r.name().as_ref(), chars.as_slice());
                 for v in gl.into_iter() {
                     r.add_glyph(v.id, v);
